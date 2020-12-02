dc1193eb2d912a3e2cd4a95b4c723884600b7b9f
manasjain0405 <manasjain111@gmail.com>
2020-12-02 22:55:38 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Projects from './components/Projects'
 import Resume from './components/Resume'
 import Home from './components/Home'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab, faGithubSquare, faLinkedin, faMedium, faTwitterSquare} from '@fortawesome/free-brands-svg-icons'
+import { fas, faEnvelopeSquare, faCode, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, fas, faLinkedin, faTwitterSquare, faGithubSquare, faEnvelopeSquare, faMedium, faCode, faMapMarkerAlt);
 
 function App() {
   return (
-    <div className="App">
+    <div className="App dark">
       <Router basename='/'>
-        <Navigation />
-        <Switch>
+      <Navigation />
+      <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/projects" component={Projects} />
         <Route path="/resume" component={Resume} />
-        </Switch>
+      </Switch>
       </Router>
     </div>
   );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import './Profile.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Profile(props) {
+    const preurl = "https://www.";
+    const item = props.dataParentToChild;
+    return (
+        <div>
+            <Image roundedCircle fluid src={item.avatar_url} />
+            <br />
+            <div className="details">
+            <h2> {item.name} </h2>
+            <p> {item.bio} </p>
+            </div>
+            <div className="contact-details">
+                <p> <FontAwesomeIcon className="contact-font" icon="map-marker-alt" aria-hidden="true" /> {item.location} </p>
+                <p> <FontAwesomeIcon className="contact-font" icon="envelope-square" aria-hidden="true" />
+                    <a href={"mailto:" + process.env.REACT_APP_EMAIL}>{process.env.REACT_APP_EMAIL} </a> </p>
+                <p> <FontAwesomeIcon className="contact-font" icon={['fab', 'github-square']} aria-hidden="true" />
+                    <a href={preurl + process.env.REACT_APP_GITHUB} target="_blank" rel="noreferrer">{process.env.REACT_APP_GITHUB} </a> </p>
+                <p> <FontAwesomeIcon className="contact-font" icon={['fab', 'linkedin']} aria-hidden="true" />
+                    <a href={preurl + process.env.REACT_APP_LINKEDIN} target="_blank" rel="noreferrer">{process.env.REACT_APP_LINKEDIN} </a> </p>
+                <p> <FontAwesomeIcon className="contact-font" icon={['fab', 'twitter-square']} aria-hidden="true" />
+                    <a href={preurl + process.env.REACT_APP_TWITTER} target="_blank" rel="noreferrer">{process.env.REACT_APP_TWITTER} </a> </p>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
